d1246f29f28edefb6bcfe0fba5cf32ef1a94daef
Eshlyne <eshlyne28@gmail.com>
2023-10-19 21:39:08 +0800

    added init fn call in main.c

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include "cprocessing.h"
 #include "menu.h"
+#include "config.h"
 
 
 
 int main(void)
 {
+	init();
 	//CP_Engine_SetNextGameState(splash_screen_init, splash_screen_update, splash_screen_exit);
 	CP_Engine_SetNextGameState(menuInit, menuUpdate, menuExit);
 	CP_Engine_Run();
