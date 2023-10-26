f7e62d52be3ee4e5d4bdb8e8f2d0ee8b0f3e2718
jspoh <jingsengyeah@gmail.com>
2023-10-26 22:08:48 +0800

    tu16: wip - going to test so committing

--- utils.c
+++ utils.c
@@ -1,4 +1,5 @@
 #include "utils.h"
+#include <math.h>
 
 
 void drawRect(Rect* r, CP_Color* fillColor, CP_Color* strokeColor) {
@@ -7,7 +8,19 @@ void drawRect(Rect* r, CP_Color* fillColor, CP_Color* strokeColor) {
 	CP_Graphics_DrawRect(r->pos.x, r->pos.y, r->size.width, r->size.height);
 }
 
-void drawRectAdvanced(Rect* r, CP_Color* fillColor, CP_Color* strokeColor, Position* center) {
+float getDistance(float x1, float y1, float x2, float y2) {
+	return sqrt(pow(x2 - x1, 2.0) + pow(y2 - y1, 2.0));
+}
+
+void drawRectAdvanced(Rect* r, CP_Color* fillColor, CP_Color* strokeColor, Position* pivot) {
+	float radius = getDistance(r->pos.x, r->pos.y, pivot->x, pivot->y);
+
+	float newX = radius * cos(r->pos.direction);
+	float newY = radius * sin(r->pos.direction);
+
+	r->pos.x += newX;
+	r->pos.y -= newY;
+
 	CP_Settings_Fill(*fillColor);
 	CP_Settings_Stroke(*strokeColor);
 	CP_Graphics_DrawRectAdvanced(r->pos.x, r->pos.y, r->size.width, r->size.height, 0, 0);
