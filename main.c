ae84b775b598c642c9b06b8919777a0e30c693e5
jspoh <jingsengyeah@gmail.com>
2023-10-26 11:45:23 +0800

    tu23: set first launch to game for ease of development on master

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include "cprocessing.h"
-//#include "menu.h"
+#include "game.h"
 #include "config.h"
 #include "splash.h"
 #include <stdio.h>
@@ -13,8 +13,8 @@ int main(void)
 		fprintf(stderr, "App config failed to initialize\n");
 		return 1;
 	}
-	CP_Engine_SetNextGameState(splash_screen_init, splash_screen_update, splash_screen_exit);
-	//CP_Engine_SetNextGameState(menuInit, menuUpdate, menuExit);
+	//CP_Engine_SetNextGameState(splash_screen_init, splash_screen_update, splash_screen_exit);  // for production
+	CP_Engine_SetNextGameState(gameInit, gameUpdate, gameExit);  // for development
 	CP_Engine_Run();
 	return 0;
 }
