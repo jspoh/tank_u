ec4fbd14c99e1f635e3027f71ab1239408610125
jspoh <jingsengyeah@gmail.com>
2023-11-05 01:24:37 +0800

    misc code reformatting

--- tank_utils.c
+++ tank_utils.c
@@ -3,26 +3,6 @@
 #include <stdio.h>
 
 
-double getDistance(double x1, double y1, double x2, double y2) {
-	return sqrt(pow(x2 - x1, 2.0) + pow(y2 - y1, 2.0));
-}
-
-double dotProduct(Vector v, Vector u) {
-	return v.x * u.x + v.y * u.y;
-}
-
-double magnitude(Vector v) {
-	return sqrt(pow(v.x, 2.0) + pow(v.y, 2.0));
-}
-
-double radiansToDegrees(double radians) {
-	return radians * 180 / M_PI;
-}
-
-double degreesToRadians(double degrees) {
-	return degrees * M_PI / 180;
-}
-
 /**
 * @brief			Generally, the vector OT will be passed in as an argument
 *
@@ -33,7 +13,7 @@ double degreesToRadians(double degrees) {
 * @param v			generally will be OT (origin to top where origin refers to center of rect)
 * @param degrees	clockwise CHANGE in degrees of rotation
 */
-Vector rotateVectorClockwise(Vector v, double degrees) {
+Vector _rotateVectorClockwise(Vector v, double degrees) {
 	Vector u = { 0 };
 	double radians = degreesToRadians(degrees);
 	u.x = -(cos(radians) * v.x + sin(radians) * v.y);
@@ -45,7 +25,7 @@ Vector rotateVectorClockwise(Vector v, double degrees) {
 * @brief Position pos + Vector v
 *
 */
-Position translatePosition(Position pos, Vector v) {
+Position _translatePosition(Position pos, Vector v) {
 	Position result = { pos.x + v.x, pos.y + v.y };
 	return result;
 }
@@ -55,13 +35,13 @@ Position translatePosition(Position pos, Vector v) {
 */
 Vector getDVector(Tank* t) {
 	const Vector OT = { 0,-1 };  // vector facing upwards
-	return rotateVectorClockwise(OT, t->pos.direction);
+	return _rotateVectorClockwise(OT, t->pos.direction);
 }
 
 /**
 * !TODO: might have issues
 */
-Position getTankCenterCorner(Tank* t) {
+Position __getTankCenterCorner(Tank* t) {
 	const Position TL = t->pos;
 
 	/* get rect center */
@@ -73,12 +53,12 @@ Position getTankCenterCorner(Tank* t) {
 	double scalar = t->size.height / 2;
 	Vector v = { scalar * -currentDirection.x, scalar * -currentDirection.y };
 	// ML refers to midpoint of the left side of the rectangle
-	const Position ML = translatePosition(TL, v);
+	const Position ML = _translatePosition(TL, v);
 
 	scalar = t->size.width / 2;
 	v.x = scalar * n.x;
 	v.y = scalar * n.y;
-	const Position O = translatePosition(ML, v);
+	const Position O = _translatePosition(ML, v);
 	//printf("%f, %f @ %f degrees\n", O.x, O.y, r->pos.direction);  // correct here (testing with 45deg rotation) expected 1061.88, 508.84 @ 45 degrees
 	/* end get rect center */
 
@@ -87,7 +67,7 @@ Position getTankCenterCorner(Tank* t) {
 
 
 /* when using CP_POSITION_CENTER */
-Position getTurretPos(Tank* t, Size turretSize) {
+Position _getTurretPos(Tank* t, Size turretSize) {
 	double scalar = 3 + sqrt(pow(t->size.width / 2.0, 2.0) + pow(t->size.height / 2.0, 2.0));  // center of rect to top left
 
 	Position O = { 0 };
@@ -103,7 +83,7 @@ Position getTurretPos(Tank* t, Size turretSize) {
 	return O;
 }
 
-double getModifierAngle(Tank* t, Position rectTL) {
+double __getModifierAngle(Tank* t, Position rectTL) {
 	Position O = { t->pos.x, t->pos.y };
 	Vector OTL = {
 		rectTL.x - O.x,
@@ -121,22 +101,7 @@ double getModifierAngle(Tank* t, Position rectTL) {
 }
 
 /**
- * @brief	Implemtation method:
- *			let O be center of rect(and the circle the top left corner creates when pivoting on the rect's center
- *			let TL be the top left corner of the rectangle
- *			let TR be the top right corner of the rectangle
- *			let BL be the bottom left corner of the rectangle
- *			let BR be the bottom right corner of the rectangle
- *			let radius be radius of the aforementioned circle
- *			let w be width of the rectangle
- *			let h be height of the rectangle
- *			let defaultAngle be the angle used for calculation when rect is straight (when rect.pos.direction is 0)
- *
- *			using formula
- *			x = r cos(angle)
- *			y = r sin(angle)
- *
- *			refer to math workings for more clarification if required
+ * @brief
  *
  *
 */
@@ -162,12 +127,11 @@ void drawTankAdvanced(Tank* t, CP_Color* fillColor, CP_Color* strokeColor) {
 
 	size.width *= 0.3;
 	size.height *= 0.6;
-	Position O = getTurretPos(t, size);
+	Position O = _getTurretPos(t, size);
 
 	pos.x = O.x;
 	pos.y = O.y;
 
 
 	CP_Graphics_DrawRectAdvanced((float)pos.x, (float)pos.y, (float)size.width, (float)size.height, (float)t->pos.direction, 0.f);
-	CP_Settings_RectMode(CP_POSITION_CENTER);
 }
