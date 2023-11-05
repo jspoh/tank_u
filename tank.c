f69ec4bdd42998c8a7006bf82124dcf8883ea7d6
js <99598950+jspoh@users.noreply.github.com>
2023-11-05 20:00:09 +0800

    tu80: slowed tank's reverse speed to half of forwards max speed (#21)

--- tank.c
+++ tank.c
@@ -51,7 +51,12 @@ void _moveTanks(void) {
 		/*movement*/
 		if (CP_Input_KeyDown(keybindings[i].up) || CP_Input_KeyDown(keybindings[i].down)) {
 			t->speed += ACCELERATION * dt;  // add speed
-			t->speed = t->speed > MOVEMENT_SPEED ? MOVEMENT_SPEED : t->speed;  // limit speed to MOVEMENT_SPEED
+			if (t->currentDir == BACK) {  // limit speed to half of max speed if reversing
+				t->speed = t->speed > MOVEMENT_SPEED / 2 ? MOVEMENT_SPEED / 2 : t->speed;
+			}
+			else {  // limit movement speed to max speed if going forwards
+				t->speed = t->speed > MOVEMENT_SPEED ? MOVEMENT_SPEED : t->speed;  // limit speed to MOVEMENT_SPEED
+			}
 			const double distance = dt * t->speed;
 
 			if (CP_Input_KeyDown(keybindings[i].up)) {
