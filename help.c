03233258a5cfa5899d8515b6d97a4b3d52ed4898
jspoh <jingsengyeah@gmail.com>
2023-11-05 19:52:48 +0800

    tu77: added backdrop and back btn to all menu options

--- help.c
+++ help.c
@@ -1,6 +1,19 @@
+#include "backdrop.h"
+#include "config.h"
+#include "back_btn.h"
+#include "menu.h"
 
+extern int menuState;
 
 
 void renderHelp(void) {
+	renderBackdrop();
 
+	/*help screen stuff goes here*/
+
+	bool isBackClicked = renderBackButton();
+
+	if (isBackClicked) {
+		menuState = MENU_PAGE;
+	}
 }
--- options.c
+++ options.c
@@ -1,8 +1,19 @@
-#include "config.h"
-#include "cprocessing.h"
 #include "backdrop.h"
+#include "config.h"
+#include "back_btn.h"
+#include "menu.h"
+
+extern int menuState;
 
 
 void renderOptions(void) {
 	renderBackdrop();
+
+	/*options screen stuff goes here*/
+
+	bool isBackClicked = renderBackButton();
+
+	if (isBackClicked) {
+		menuState = MENU_PAGE;
+	}
 }
