4c8bd9d86a9e616dab38cb4da9b3a6b53a802958
twxinnn <twxinnn@gmail.com>
2023-11-09 01:15:51 +0800

    WIP. created drawHealthBar function

--- tank.c
+++ tank.c
@@ -23,11 +23,43 @@ Size tankSize = { 75.f, 100.f };
 
 extern Keybinds keybindings[];
 
+// HARD CODING CERTAIN STUFF FOR NOW!!!!
+void drawHealthBar(Tank* tank, int playerIndex) {
+	CP_Color fillCol = CP_Color_Create(0, 255, 0, 255); // Green colour for health
+	CP_Color strokeCol = CP_Color_Create(0, 0, 0, 255); // Black colour for border
+
+	// Calculate percentage of health remaining
+	double healthPercentage = (tank->health / MAX_HEALTH) * 100.0;
+
+	// Calculate width of the health bar based on the tank's health
+	double barWidth = tank->size.width * (healthPercentage / 100.0);
+	double barHeight = 10;
+	//Size size = { barWidth, barHeight };
+	double xPos, yPos;
+	// Calculate position based on player index
+	if (playerIndex == 0)
+	{
+		xPos = 10; // Adjust x-position for player 1
+	}
+	else
+	{
+		xPos = WINDOW_SIZE.width - barWidth - 10; // Adjust x position for player 2
+	}
+
+	yPos = 50;
+
+	//Position barPos = { xPos, yPos };
+	//drawRect(&barPos, &size, &fillCol, &strokeCol);
+	CP_Settings_Fill(fillCol);
+	CP_Settings_Stroke(strokeCol);
+	CP_Graphics_DrawRect(xPos, yPos, barWidth, barHeight);
+
+}
+
 void _drawTank(Tank* tank) {
 	CP_Color fillCol = CP_Color_Create(tank->color.r, tank->color.g, tank->color.b, tank->color.a);
 	CP_Color strokeCol = CP_Color_Create(0, 0, 0, 255);
 	drawTankAdvanced(tank, &fillCol, &strokeCol);
-
 }
 
 void _setTankColor(Tank* tank, BYTE r, BYTE g, BYTE b, BYTE a) {
@@ -88,7 +120,7 @@ void _moveTanks(void) {
 			}
 		}
 		else {
-			t->speed -= DECELERATION * dt; 
+			t->speed -= DECELERATION * dt;
 			t->speed = t->speed < 0 ? 0 : t->speed;  // limit speed to MOVEMENT_SPEED
 			const double distance = dt * t->speed;
 
@@ -127,7 +159,7 @@ void _moveTanks(void) {
 					t->pos.direction -= dDegrees / 2;
 					t->pos.dDir = -(dDegrees / 2);
 				}
-			} 
+			}
 			else {
 				t->pos.direction += dDegrees;
 				t->pos.dDir = dDegrees;
@@ -146,13 +178,13 @@ Tank _tankConstructor(Position pos, Color color) {
 	tank.color = color;
 	tank.health = MAX_HEALTH;
 	tank.size = tankSize;
-			   
+
 	/* add tank to tanks array */
 	bool valid = false;
 	for (int i = 0; i < NUM_PLAYERS; i++) {
 		if (tanks[i].pos.x == 0.f) {
 			tanks[i] = tank;
-			valid = true; 
+			valid = true;
 			break;
 		}
 	}
@@ -175,6 +207,13 @@ void _renderTank(void) {
 		_drawTank(&tanks[i]);
 	}
 }
+
+void _renderHealthBar(void) {
+	for (int i = 0; i < NUM_PLAYERS; i++) {
+		drawHealthBar(&tanks[i], i);
+	}
+}
+
 
 void _damageTank(Tank* tank, double damage) {
 	tank->health -= damage;
@@ -224,7 +263,7 @@ void _actionTank(void) {
 			//printf("height: %lf", size.height);
 			size.width = (tanks[i].size.width * 0.6f) * 0.3f;
 			//printf("width: %lf", size.width);
-			Position turretTip= _getTurretCenter(&tanks[i], size);
+			Position turretTip = _getTurretCenter(&tanks[i], size);
 
 			onFireCannonball(turretTip, unitVector, i);
 			// anws, you only need 1 printline for this. no need to check what the value of `i` is
@@ -249,14 +288,15 @@ void _actionTank(void) {
 
 
 void initTank(void) {
-	_createTank(WINDOW_SIZE.width/6, WINDOW_SIZE.height/2, 90.f, 0, 255, 0, 255);
-	_createTank(WINDOW_SIZE.width/6*5, WINDOW_SIZE.height/2, 270.f, 255, 0, 0, 255);
+	_createTank(WINDOW_SIZE.width / 6, WINDOW_SIZE.height / 2, 90.f, 0, 255, 0, 255);
+	_createTank(WINDOW_SIZE.width / 6 * 5, WINDOW_SIZE.height / 2, 270.f, 255, 0, 0, 255);
 }
 
 void updateTank(void) {
 	_moveTanks();
 	_actionTank();
 	_renderTank();
+	_renderHealthBar();
 }
 
 void destroyTank(void) {
