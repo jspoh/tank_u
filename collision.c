24f407565d42df3a3a3e552786aaef3d3979ead6
jspoh <jingsengyeah@gmail.com>
2023-11-09 19:10:04 +0800

    tank tank collision done

--- collision.c
+++ collision.c
@@ -6,14 +6,7 @@
 #include <stdio.h>
 
 bool mouseInRect(Rect r, double mouseX, double mouseY) {
-	return (mouseX >= r.pos.x && mouseX <= r.pos.x + r.size.width && mouseY >= r.pos.y && mouseY <= r.pos.y + r.size.height);
-}
-
-Vector _T(Vector v, double m[2][2], Vector r) {
-    Vector result;
-    result.x = m[0][0] * v.x + m[0][1] * v.y + r.x;
-    result.y = m[1][0] * v.x + m[1][1] * v.y + r.y;
-    return result;
+    return (mouseX >= r.pos.x && mouseX <= r.pos.x + r.size.width && mouseY >= r.pos.y && mouseY <= r.pos.y + r.size.height);
 }
 
 void _getTankCorners(Tank* t, Position* corners) {
@@ -49,35 +42,36 @@ void _getTankCorners(Tank* t, Position* corners) {
 
 
 double _min4(double a, double b, double c, double d) {
-	double m = a;
-	if (b < m) {
+    double m = a;
+    if (b < m) {
         m = b;
     }
     if (c < m) {
         m = c;
     }
-	if (d < m) {
+    if (d < m) {
         m = d;
     }
-	return m;
+    return m;
 };
 
 double _max4(double a, double b, double c, double d) {
-	double m = a;
-	if (b > m) {
-		m = b;
-	}
-	if (c > m) {
-		m = c;
-	}
-	if (d > m) {
-		m = d;
-	}
-	return m;
+    double m = a;
+    if (b > m) {
+        m = b;
+    }
+    if (c > m) {
+        m = c;
+    }
+    if (d > m) {
+        m = d;
+    }
+    return m;
 }
 
 /**
 * @brief    checks if two tanks are colliding using separation of axis theorem
+*           As we are using rectangles, instead of using 4 edges/shape, we only need to use 2 as the other 2 are parallel
 */
 bool checkTankCollision(Tank* t1, Tank* t2, Vector* d) {
     Position t1Corners[4] = { 0 };
@@ -85,6 +79,52 @@ bool checkTankCollision(Tank* t1, Tank* t2, Vector* d) {
     Position t2Corners[4] = { 0 };
     _getTankCorners(t2, t2Corners);
 
+    const int UNIQUE_EDGES = 4;
+    Vector allEdges[] = {
+        t1->pos.d,  // bottom to top
+        { t1Corners[1].x - t1Corners[0].x, t1Corners[1].y - t1Corners[0].y },  // t1 top left to top right (l -> r)
+        t2->pos.d,  // bottom to top
+        { t2Corners[1].x - t2Corners[0].x, t2Corners[1].y - t2Corners[0].y }  // t2 top left to top right (l -> r)
+    };
+
+    // iterate through edges
+    for (int i = 0; i < UNIQUE_EDGES; i++) {
+        Vector axis = allEdges[i];
+
+        double t1DotMin = 0;
+        double t1DotMax = 0;
+        double t2DotMin = 0;
+        double t2DotMax = 0;
+
+        // iterate through vertices
+        for (int j = 0; j < 4; j++) {
+            Vector t1C = { t1Corners[j].x, t1Corners[j].y };
+            Vector t2C = { t2Corners[j].x, t2Corners[j].y };
+
+            if (j == 0) {
+                t1DotMin = dotProduct(axis, t1C);
+                t1DotMax = t1DotMin;
+                t2DotMin = dotProduct(axis, t2C);
+                t2DotMax = t2DotMin;
+                continue;
+            }
+
+            double t1Dot = dotProduct(axis, t1C);
+            double t2Dot = dotProduct(axis, t2C);
+
+            t1DotMin = t1Dot < t1DotMin ? t1Dot : t1DotMin;
+            t1DotMax = t1Dot > t1DotMax ? t1Dot : t1DotMax;
+            t2DotMin = t2Dot < t2DotMin ? t2Dot : t2DotMin;
+            t2DotMax = t2Dot > t2DotMax ? t2Dot : t2DotMax;
+        }
+        if (t1DotMin > t2DotMax || t2DotMin > t1DotMax) {  // one of the projections does not overlap
+            return false;
+        }
+    }
+
+    return true;  // all projections overlap
+
+    /* // working, but not proper SAT. will return false positives if corner is near an edge
     double t1MinX = _min4(t1Corners[0].x, t1Corners[1].x, t1Corners[2].x, t1Corners[3].x);
     double t1MaxX = _max4(t1Corners[0].x, t1Corners[1].x, t1Corners[2].x, t1Corners[3].x);
     double t1MinY = _min4(t1Corners[0].y, t1Corners[1].y, t1Corners[2].y, t1Corners[3].y);
@@ -100,7 +140,8 @@ bool checkTankCollision(Tank* t1, Tank* t2, Vector* d) {
             return true;
         }
     }
-
     return false;
+    */
+
 }
 
