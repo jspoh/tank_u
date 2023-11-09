a82e7794d6c461b71422ef1338d1b05618bcba0c
jspoh <jingsengyeah@gmail.com>
2023-11-09 20:28:20 +0800

    tank wall collision done

--- collision.h
+++ collision.h
@@ -3,5 +3,6 @@
 #include "tank.h"
 
 bool mouseInRect(Rect r, double mouseX, double mouseY);
-bool areTanksColliding(Tank* t1, Tank* t2, Vector* v);
-void _getRectCorners(Rect* r, Vector d, Position* corners);  // for debugging
+bool areTanksColliding(Tank* t1, Tank* t2, Vector* collisionVector);
+void _getRectCorners(Rect* r, Vector d, Position* corners, bool usingCenter);  // for debugging
+bool colTankWall(Tank* t, Vector* collisionVector);
