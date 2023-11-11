165c2b0b2364c34babfbaebd9106e8308d192a37
twxinnn <twxinnn@gmail.com>
2023-11-11 13:21:05 +0800

    used drawRect for healthbar

--- healthbar.c
+++ healthbar.c
@@ -7,43 +7,83 @@
 
 extern Tank tanks[];
 // HARD CODING CERTAIN STUFF FOR NOW!!!!
+//void drawHealthBar(Tank* tank, int playerIndex) {
+//	CP_Color fillCol = CP_Color_Create(0, 255, 0, 255); // Green colour for health
+//	CP_Color strokeCol = CP_Color_Create(0, 0, 0, 255); // Black colour for border
+//
+//	// Calculate percentage of health remaining
+//	double healthPercentage = (tank->health / MAX_HEALTH) * 100.0;
+//
+//	// Calculate width of the health bar based on the tank's health
+//	double barWidth = 3 * (tank->size.width * (healthPercentage / 100.0));
+//	double barHeight = 30.0;
+//	//Size size = { barWidth, barHeight };
+//	double xPos, yPos;
+//	// Calculate position based on player index
+//	if (playerIndex == 0)
+//	{
+//		xPos = 10; // Adjust x-position for player 1
+//	}
+//	else
+//	{
+//		xPos = WINDOW_SIZE.width - barWidth - 10; // Adjust x position for player 2
+//	}
+//
+//	yPos = 50;
+//
+//	//Position barPos = { xPos, yPos };
+//	//drawRect(&barPos, &size, &fillCol, &strokeCol);
+//	CP_Settings_Fill(fillCol);
+//	CP_Settings_Stroke(strokeCol);
+//	CP_Graphics_DrawRect((float)xPos, (float)yPos, (float)barWidth, (float)barHeight);
+//
+//	// Display HP value
+//	CP_Settings_TextSize(25);
+//	CP_Settings_Fill(fillCol);
+//
+//	char hpText[50];
+//	snprintf(hpText, 50, "HP: %.0f", tank->health); // Convert HP value to string
+//	CP_Font_DrawText(hpText, (float)xPos + (float)barWidth / 2, (float)yPos - 20); // Draw HP text above health bar
+//	printf("tank->size.width: %f\n", tank->size.width);
+//	printf("healthPercentage: %f\n", healthPercentage);
+//}
+
 void drawHealthBar(Tank* tank, int playerIndex) {
-	CP_Color fillCol = CP_Color_Create(0, 255, 0, 255); // Green colour for health
-	CP_Color strokeCol = CP_Color_Create(0, 0, 0, 255); // Black colour for border
+    CP_Color fillCol = CP_Color_Create(0, 255, 0, 255); // Green colour for health
+    CP_Color strokeCol = CP_Color_Create(0, 0, 0, 255); // Black colour for border
 
-	// Calculate percentage of health remaining
-	double healthPercentage = (tank->health / MAX_HEALTH) * 100.0;
+    // Calculate percentage of health remaining
+    double healthPercentage = (tank->health / MAX_HEALTH) * 100.0;
 
-	// Calculate width of the health bar based on the tank's health
-	double barWidth = 3 * (tank->size.width * (healthPercentage / 100.0));
-	double barHeight = 30.0;
-	//Size size = { barWidth, barHeight };
-	double xPos, yPos;
-	// Calculate position based on player index
-	if (playerIndex == 0)
-	{
-		xPos = 10; // Adjust x-position for player 1
-	}
-	else
-	{
-		xPos = WINDOW_SIZE.width - barWidth - 10; // Adjust x position for player 2
-	}
+    // Calculate width of the health bar based on the tank's health
+    double barWidth = 3 * (tank->size.width * (healthPercentage / 100.0));
+    double barHeight = 30.0;
 
-	yPos = 50;
+    double xPos, yPos;
 
-	//Position barPos = { xPos, yPos };
-	//drawRect(&barPos, &size, &fillCol, &strokeCol);
-	CP_Settings_Fill(fillCol);
-	CP_Settings_Stroke(strokeCol);
-	CP_Graphics_DrawRect((float)xPos, (float)yPos, (float)barWidth, (float)barHeight);
+    // Calculate position based on player index
+    if (playerIndex == 0) {
+        xPos = 10; // Adjust x-position for player 1
+    }
+    else {
+        xPos = WINDOW_SIZE.width - barWidth - 10; // Adjust x position for player 2
+    }
 
-	// Display HP value
-	CP_Settings_TextSize(25);
-	CP_Settings_Fill(fillCol);
+    yPos = 50;
 
-	char hpText[50];
-	snprintf(hpText, 50, "HP: %.0f", tank->health); // Convert HP value to string
-	CP_Font_DrawText(hpText, (float)xPos + (float)barWidth / 2, (float)yPos - 20); // Draw HP text above health bar
+    Rect barRect = { {barWidth, barHeight}, {xPos, yPos} };
+    drawRect(&barRect, &fillCol, &strokeCol);
+
+    // Display HP value
+    CP_Settings_TextSize(25);
+    CP_Settings_Fill(fillCol);
+
+    char hpText[50];
+    snprintf(hpText, 50, "HP: %.0f", tank->health); // Convert HP value to string
+    CP_Font_DrawText(hpText, (float)xPos + (float)barWidth / 2, (float)yPos - 20); // Draw HP text above health bar
+    printf("tank->size.width: %f\n", tank->size.width);
+    printf("healthPercentage: %f\n", healthPercentage);
+
 }
 
 void _renderHealthBar(void) {
