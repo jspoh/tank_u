1aa7781cb63524afb6b0cc7a3f05cfe23a2c6f45
kjres5 <fookarjun01@gmail.com>
2023-11-12 17:44:29 +0800

    tu32 saving code for powerup and dropbox img

--- dropbox.c
+++ dropbox.c
@@ -20,33 +20,4 @@ void dropBox(void) {
 	dropboxImg = CP_Image_Load("Assets/powerup/4.png");
 	CP_Image_Draw(dropboxImg, dropbox.pos.x, dropbox.pos.y, dropbox.size.width, dropbox.size.height, 255);
 	/*drawRect(&dropbox, &fillCol, &strokeCol);*/
-	
 }
-
-
-
-//PowerUp damage;
-// create _doubleDamage function 
-//void _doubleDamage(void)
-//{
-//	// powerup perimeter
-//	//PowerUp.perimeter
-//	// load img of double Damage
-//	// upon key trigger (user activate) 
-//	// powerupactive(bool)
-//	// next 5 seconds
-//	// cannonball damage x2
-//}
-
-// create _shotgun function
-	// powerup perimeter
-	// load img of shotgun
-	// upon key trigger (user activate)
-	// powerupactive(bool)
-// create _rapidFire function
-
-// create _spawnPowerup function
-	// start of game spawn 1 randomised powerup (randomize)
-		// call whichever powerup it has chosen
-	// Upon collection, fixed 30 seconds to spawn next randomised powerup (wait 30 sec, randomize)
-		// call whichever powerup it has chosen
--- powerup.c
+++ powerup.c
@@ -3,6 +3,16 @@
 #include "game.h"
 #include <stdio.h>
 #include "powerup.h"
+#include "cannonball.h"
 
 // now that drop box has been drawn with its image
 // create function that randomizes between the 3 powerups from cannonball.c using extern(?)
+
+
+
+//void randPowerup(void)
+//{
+//	// randomize between the 3 powerups
+//	
+//}
+//
