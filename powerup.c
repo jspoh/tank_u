7c9707fd04cd96d18f1ea01a10f250d0a056909d
kjres5 <fookarjun01@gmail.com>
2023-11-12 16:15:11 +0800

    tu32 shifted code to branch with dropbox image loaded at pos and size

---
+++ powerup.c
@@ -0,0 +1,8 @@
+#include "cprocessing.h"
+#include "config.h"
+#include "game.h"
+#include <stdio.h>
+#include "powerup.h"
+
+// now that drop box has been drawn with its image
+// create function that randomizes between the 3 powerups from cannonball.c using extern(?)
