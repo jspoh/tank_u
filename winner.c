5c243ad560a9d6000d3d41734315266c5430e778
Merylene <143251588+Merylene@users.noreply.github.com>
2023-11-12 15:43:48 +0800

    changed the sound settings to not loop the audio

--- winner.c
+++ winner.c
@@ -100,7 +100,7 @@ void winnerInit(void) {
 
 void winnerUpdate(void) {
 	if (!playing) {
-		CP_Sound_PlayAdvanced(winAudio, (float)sfxVolume, 1.f, true, SFX_GROUP);
+		CP_Sound_PlayAdvanced(winAudio, (float)sfxVolume, 1.f, false, SFX_GROUP);
 		playing = true;
 	}
 	buttonConstructor();
