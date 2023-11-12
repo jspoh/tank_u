11c97b457b9dcc30f95b8ae8a2eb18ee064dcc59
jspoh <jingsengyeah@gmail.com>
2023-11-12 16:18:40 +0800

    main menu done

--- credits.c
+++ credits.c
@@ -5,30 +5,30 @@
 #include <stdio.h>
 
 
-#define numCreditsPngs 2
-CP_Image creditsPngs[numCreditsPngs] = { 0 };
+#define numCreditsPages 2
+CP_Image creditsImgs[numCreditsPages] = { 0 };
 enum PAGE_NUMS creditsPage = PAGE_1;
 extern int menuState;
 
 void destroyCredits(void) {
-	for (int i=0; i<numCreditsPngs; i++) {
-		CP_Image_Free(&creditsPngs[i]);
+	for (int i=0; i<numCreditsPages; i++) {
+		CP_Image_Free(&creditsImgs[i]);
 	}
 	creditsPage = PAGE_1;
 }
 
 void renderCredits(void) {
-	for (int i=0; i<numCreditsPngs; i++) {
-		if (creditsPngs[i] == NULL) {
+	for (int i=0; i<numCreditsPages; i++) {
+		if (creditsImgs[i] == NULL) {
 			char path[MAX] = "";
 			snprintf(path, MAX, "Assets/menu/credits/credits_%d.png", i+1);
 			// puts(path);
-			creditsPngs[i] = CP_Image_Load(path);
+			creditsImgs[i] = CP_Image_Load(path);
 		}
 	}
 
 	renderBackdrop();
-	CP_Image_Draw(creditsPngs[creditsPage], (float)(WINDOW_SIZE.width / 2), (float)(WINDOW_SIZE.height / 2), (float)CP_Image_GetWidth(creditsPngs[creditsPage]), (float)CP_Image_GetHeight(creditsPngs[creditsPage]), 255);
+	CP_Image_Draw(creditsImgs[creditsPage], (float)(WINDOW_SIZE.width / 2), (float)(WINDOW_SIZE.height / 2), (float)CP_Image_GetWidth(creditsImgs[creditsPage]), (float)CP_Image_GetHeight(creditsImgs[creditsPage]), 255);
 
 	bool isBackClicked = renderBackButton();
 	if (isBackClicked) {
@@ -36,10 +36,10 @@ void renderCredits(void) {
 		creditsPage = PAGE_1;
 	}
 
-	if (creditsPage < numCreditsPngs - 1) {
+	if (creditsPage < numCreditsPages - 1) {
 		bool isNextClicked = renderNextButton();
 		if (isNextClicked) {
-			creditsPage = PAGE_2;
+			creditsPage++;
 		}
 	}
 }
--- help.c
+++ help.c
@@ -2,18 +2,45 @@
 #include "config.h"
 #include "action_btn.h"
 #include "menu.h"
+#include <stdio.h>
 
+#define numHelpPages 2
+CP_Image helpImgs[numHelpPages] = { 0 };
+enum PAGE_NUMS helpPage = PAGE_1;
 extern int menuState;
 
 
-void renderHelp(void) {
-	renderBackdrop();
+void destroyHelp(void) {
+	for (int i=0; i<numHelpPages; i++) {
+		CP_Image_Free(&helpImgs[i]);
+	}
+	helpPage = PAGE_1;
+}
 
-	/*help screen stuff goes here*/
+void renderHelp(void) {
+	for (int i=0; i<numHelpPages; i++) {
+		if (helpImgs[i] == NULL) {
+			char path[MAX] = "";
+			snprintf(path, MAX, "Assets/menu/help/help_%d.png", i+1);
+			// puts(path);
+			helpImgs[i] = CP_Image_Load(path);
+		}
+	}
+
+	renderBackdrop();
+	CP_Image_Draw(helpImgs[helpPage], (float)(WINDOW_SIZE.width / 2), (float)(WINDOW_SIZE.height / 2), (float)CP_Image_GetWidth(helpImgs[helpPage]), (float)CP_Image_GetHeight(helpImgs[helpPage]), 255);
 
 	bool isBackClicked = renderBackButton();
 
 	if (isBackClicked) {
 		menuState = MENU_PAGE;
+		helpPage = PAGE_1;
+	}
+
+	if (helpPage < numHelpPages - 1) {
+		bool isNextClicked = renderNextButton();
+		if (isNextClicked) {
+			helpPage++;
+		}
 	}
 }
