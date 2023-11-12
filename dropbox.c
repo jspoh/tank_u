63871c649be3a5f2adfadaddc567c97786850eaf
Alaska52 <134671437+Alaska52@users.noreply.github.com>
2023-11-12 23:07:58 +0800

    Update dropbox.c

--- dropbox.c
+++ dropbox.c
@@ -5,6 +5,11 @@
 #include "dropbox.h"
 CP_Image dropboxImg;
 
+// size of the dropbox
+static Size size = { 50.0, 50.0 };
+// position of the wall
+static Position pos = { (WINDOW_SIZE.width / 2), (WINDOW_SIZE.height / 2) };
+
 
 Rect dropbox = {0};
 void initDropbox(void)
@@ -12,10 +17,6 @@ void initDropbox(void)
 	dropboxImg = CP_Image_Load("Assets/powerup/4.png");
 }
 void renderDropbox(void) {
-	// size of the dropbox
-	Size size = { 50, 50 };
-	// position of the wall
-	Position pos = { (WINDOW_SIZE.width / 2), (WINDOW_SIZE.height / 2) };
 	dropbox.size = size;
 	dropbox.pos = pos;
 	// so now instead of colour I load img of dropbox from asset
@@ -26,7 +27,6 @@ void renderDropbox(void) {
 	/*drawRect(&dropbox, &fillCol, &strokeCol);*/
 }
 
-
 void destroyDropbox(void) {
-		CP_Image_Free(&dropboxImg);
-	}
+	CP_Image_Free(&dropboxImg);
+}
