f3f2cc423b863749eb9babd969ef3ec5c6dd94aa
kjres5 <fookarjun01@gmail.com>
2023-11-12 16:07:56 +0800

    tu32 shifted code to branch with dropbox image loaded at pos and size

--- dropbox.c
+++ dropbox.c
@@ -3,27 +3,24 @@
 #include "game.h"
 #include <stdio.h>
 #include "dropbox.h"
+CP_Image dropboxImg;
 
 
-//void drawDropBox(void)
-//{
-//	// load img
-//	CP_Image_Draw()
-//}
-// create a draw dropbox function
+
 void dropBox(void) {
 	// size of the dropbox
 	Size size = { 50, 50 };
 	// position of the wall
-	Position pos = { (WINDOW_SIZE.width / 2)-25, (WINDOW_SIZE.height / 2)-25 };
+	Position pos = { (WINDOW_SIZE.width / 2), (WINDOW_SIZE.height / 2) };
 	Dropbox dropbox = { size, pos };
 	// so now instead of colour I load img of dropbox from asset
 	CP_Color fillCol = CP_Color_Create(100, 100, 100, 255);
 	CP_Color strokeCol = CP_Color_Create(0, 0, 0, 255);
-	drawRect(&dropbox, &fillCol, &strokeCol);
-
-	// now that i have the dropbox size and position i need do x`
-
+	// load img and draw image at pos and size of dropbox
+	dropboxImg = CP_Image_Load("Assets/powerup/4.png");
+	CP_Image_Draw(dropboxImg, dropbox.pos.x, dropbox.pos.y, dropbox.size.width, dropbox.size.height, 255);
+	/*drawRect(&dropbox, &fillCol, &strokeCol);*/
+	
 }
 
 
