d62e4a98ed124b63bab5bc498ff7585ac2b2dc5d
twxinnn <twxinnn@gmail.com>
2023-11-20 18:20:19 +0800

    documentation for healthbar and pause

--- healthbar.h
+++ healthbar.h
@@ -1,7 +1,7 @@
 /**
  * @file healthbar.h
- * @author your name (you@domain.com)
- * @brief 
+ * @author TEO WENXIN (wenxin.teo@digipen.edu)
+ * @brief this file is used to declare functions initHealthBar, updateHealthBar and destroyHealthbar to be used in other files.
  * 
  * @copyright Copyright (c) 2023
  * 
--- pause.h
+++ pause.h
@@ -1,7 +1,7 @@
 /**
  * @file pause.h
  * @author TEO WENXIN (wenxin.teo@digipen.edu)
- * @brief this file is used to declare the renderPause function
+ * @brief this file is used to declare the renderPause function to be used in other files
  * 
  * @copyright Copyright (c) 2023
  * 
